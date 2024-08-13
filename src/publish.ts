c85e5b56feef328118a611bafc8c6d940c712e9f
Phil Stenning <philstenning@hotmail.com>
2024-08-13 14:46:36 +0100

    update script

--- src/publish.ts
+++ src/publish.ts
@@ -1,2 +1,21 @@
+import { sha256 } from 'js-sha256'
+
 console.log(process.env.EMAILS ?? 'my-emails are secret')
-console.log('working');
+const emails = process.env.EMAILS ?? 'my-emails are secret'
+console.log('working')
+
+function getTempEmailAddresses() {
+  return emails.split('\r\n') // this is for windows check in github secrets
+}
+
+function hashEmails(emails: string[]) {
+  return emails.map((email) => sha256(email.trim()))
+}
+
+async function main() {
+  const emails = getTempEmailAddresses()
+  const hashedEmails = hashEmails(emails)
+  hashedEmails.forEach((email) => console.log(email))
+}
+
+main().catch(console.error)
